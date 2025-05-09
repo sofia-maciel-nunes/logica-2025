619f8f7f3ba6c607e3dd211931ec86552ed02053
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-09 14:35:02 -0300

    1010.c

---
+++ 1010.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int cod1, cod2, qtd1, qtd2;
+    float valor1, valor2, total;
+
+   
+    scanf("%d %d %f", &cod1, &qtd1, &valor1);
+
+    
+    scanf("%d %d %f", &cod2, &qtd2, &valor2);
+
+    
+    total = (qtd1 * valor1) + (qtd2 * valor2);
+
+    
+    printf("VALOR A PAGAR: R$ %.2f\n", total);
+    return 0;
+}
