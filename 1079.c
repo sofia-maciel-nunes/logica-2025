0119b4de34bf4bc4fcfbaf38745bc4afbc9df487
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-09 14:17:04 -0300

    1079.c

---
+++ 1079.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+    float A,B,C;
+    scanf("%f%f%f",&A,&B,&C);
+    float media=(A* 2 + B * 3 + C * 5) / (2 + 3 + 5);
+    printf("%.1f\n", media);
+    }
+  return 0;
+}
