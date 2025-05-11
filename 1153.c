61735768ae44322becc4ce2b036dc85ecd879912
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-11 20:26:03 -0300

    1153.c

--- 1153.c
+++ 1153.c
@@ -3,10 +3,14 @@
 int main() {
     int N;
     int fatorial = 1; 
+ 
     scanf("%d", &N);
+ 
     for (int i = 1; i <= N; i++) {
-        fatorial *= i;
+         fatorial *= i;
     }
+
     printf("%d\n", fatorial);
+ 
     return 0;
 }
