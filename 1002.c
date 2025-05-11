9633aecda27f9afb076dc8c05d9841ac228468d0
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-11 20:01:43 -0300

    1002.c

--- 1002.c
+++ 1002.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
  
 int main() {
- 
     double raio, area;
     const double pi = 3.14159;
+ 
     scanf("%lf", &raio);
+ 
     area = pi * raio * raio;
+ 
     printf("A=%.4lf\n", area);
+ 
     return 0;
 }
