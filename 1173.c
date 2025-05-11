ebeea61aff372d189a67608a5af5292d8151b557
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-11 20:32:22 -0300

    1173.c

--- 1173.c
+++ 1173.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
- int main(){
+
+ int main() {
      int N [10];
      int valor;
+  
      printf("digite um valor\n");
+  
      scanf("%d",&valor);
-     N[0]=valor;
-     for(int i=1;i<10;i++){
-     N[i]=N[i-1]*2;
+  
+     N[0] = valor;
+  
+     for(int i = 1; i < 10; i++) {
+         N[i] = N[i - 1] * 2;
  }
-     for(int i=0;i<10;i++){
-         printf("N[%d]=%d\n",i,N[i]);
+     for(int i = 0; i < 10; i++) {
+         printf("N[%d] = %d\n", i, N[i]);
      }
 
     return 0;
