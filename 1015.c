f8cf87b08ebf40a8ffde93ec0fe1a1bf34406134
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-11 20:08:26 -0300

    1015.c

--- 1015.c
+++ 1015.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <math.h> 
-int main() {
-    
-    float x1,y1;
-    float x2,y2;
+
+int main() {  
+    float x1, y1;
+    float x2, y2;
     float distancia;
-    scanf("%f %f",&x1,&y1);
-    scanf("%f %f",&x2,&y2);
-    distancia=sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    printf("%.4f\n",distancia);
+    
+    scanf("%f %f", &x1, &y1);
+    scanf("%f %f", &x2, &y2);
+    
+    distancia = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    
+    printf("%.4f\n", distancia);
   
     return 0;
 }
