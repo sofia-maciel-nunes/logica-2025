9d79a10ceed4230a6d3b55ad7a63ff5f2480e5c3
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-11 20:13:56 -0300

    1018.c

--- 1018.c
+++ 1018.c
@@ -2,14 +2,19 @@
  
 int main() {
     int N;
-    scanf("%d",&N);
-    printf("%d\n",N);
-    int notas[]={100,50,20,10,5,2,1};
-    for (int i=0;i<7;i++) {
-        int qtd_notas=N/notas[i];
-        N = N % notas[i];
-        printf("%d nota(s) de R$ %d,00\n", qtd_notas, notas[i]);
+ 
+    scanf("%d", &N);
+ 
+    printf("%d\n", N);
+ 
+    int notas[] = {100, 50, 20, 10, 5, 2, 1};
+ 
+    for (int i = 0; i < 7; i++) {
+         int qtd_notas = N/notas[i];
+         N = N % notas[i];
+     
+         printf("%d nota(s) de R$ %d,00\n", qtd_notas, notas[i]);
     }
-
+ 
     return 0;
 }
