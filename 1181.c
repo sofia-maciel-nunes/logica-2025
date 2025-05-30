ff9b4debcca3ff7f3f19aee7f78910f87f990092
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-05-30 15:17:18 -0300

    1181.c

---
+++ 1181.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int L;          
+    char T;         
+    double M[12][12], resultado = 0.0;
+
+    // Leitura da linha L e da operação T
+    scanf("%d", &L);
+    scanf(" %c", &T);  
+
+    // Leitura da matriz
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+   
+    for (int j = 0; j < 12; j++) {
+        resultado += M[L][j];
+    }
+
+   
+    if (T == 'M') {
+        resultado /= 12.0;
+    }
+
+    
+    printf("%.1lf\n", resultado);
+
+    return 0;
+}
