c34efbdc2f95c4bab575c34dd7a3179d0f4d1bba
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-06-18 10:31:50 -0300

    1003.c

---
+++ 1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    int soma;
+
+    printf("Digite dois números inteiros: ");
+    scanf("%d %d", &A, &B);
+
+    soma = A + B;
+
+    printf("A soma é %d\n", soma);
+
+    return 0;
+}
