c2364c4d44eeb1570af6889ec41008916bfd1c30
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-06-18 11:32:31 -0300

    1185.c

--- 1185.c
+++ 1185.c
@@ -4,17 +4,14 @@ int main() {
     char O;
     double M[12][12], resultado = 0.0;
     int contador = 0;
-
     
     scanf(" %c", &O);  
-
 
     for (int i = 0; i < 12; i++) {
         for (int j = 0; j < 12; j++) {
             scanf("%lf", &M[i][j]);
         }
     }
-
  
     for (int i = 0; i < 12; i++) {
         for (int j = 0; j < 11 - i; j++) {  
@@ -22,13 +19,11 @@ int main() {
             contador++;
         }
     }
-
     
     if (O == 'M') {
         resultado /= contador;
     }
 
-   
     printf("%.1lf\n", resultado);
 
     return 0;
