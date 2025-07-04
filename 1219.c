5e4be3713305e9cbbd0a6488b8fed6c42e2aced6
sofia-maciel-nunes <nunes.sofia@unifesp.br>
2025-07-04 13:57:34 -0300

    1219.c

--- 1219.c
+++ 1219.c
@@ -10,10 +10,10 @@ int main() {
         double area_triangle = sqrt(s * (s - a) * (s - b) * (s - c));
         
         double r = area_triangle/s;
-        double R = (a*b*c)/(4.0*area_triangle);
+        double R = (a * b * c)/(4.0 * area_triangle);
         
-        double area_incircle=PI*r*r;
-        double area_circumcircle=PI*R*R;
+        double area_incircle = PI*r*r;
+        double area_circumcircle = PI * R * R;
         
         double amarelo = area_circumcircle-area_triangle;
         double azul = area_triangle-area_incircle;
